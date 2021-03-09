c87e632597bbbc37b26ed0c812003a8f43c1ce4e
Quank123WIP <quank123wip@outlook.com>
2021-03-09 17:23:55 +0800

    fix: wrong css definition.

--- src/components/HomeProblemNew.vue
+++ src/components/HomeProblemNew.vue
@@ -77,7 +77,7 @@
 export default {};
 </script>
 
-<style>
+<style scoped>
 .title-icon {
     color: currentColor!important;
 }
--- src/components/HomeQuestion.vue
+++ src/components/HomeQuestion.vue
@@ -55,7 +55,7 @@
 export default {};
 </script>
 
-<style>
+<style scoped>
 .title-icon {
     color: currentColor!important;
 }
